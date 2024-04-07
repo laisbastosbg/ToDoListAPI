cc434eb36f7df0289a823d0f175391a50d714b5d
Lais Godinho <47223072+laisbastosbg@users.noreply.github.com>
2024-04-06 23:27:42 -0300

    :wrench: chore: Altera a porta de escuta para usar process.env.PORT caso exista ou 3331 caso não

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = Fastify({
 server.register(userRouter)
 server.register(taskRouter)
 
-server.listen({ port: 3331 }, function (err, address) {
+server.listen({ port: process.env.PORT ?? 3331 }, function (err, address) {
   if (err) {
     server.log.error(err)
     process.exit(1)
